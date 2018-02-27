b5fb95f915ffe37cb9925389a87ea67930a32362
Vishal <vishal.koosuri1@marist.edu>
2018-02-27 15:24:34 +0000

    Refactored and cleaned up the code

--- popup.js
+++ popup.js
@@ -1,33 +1,12 @@
-var subListBtn = document.querySelector(".get-sub-list");
-var favSubsBtn = document.querySelector("#favs");
-var courseListUl = document.querySelector('#list');
-var userName = document.querySelector("#username");
-var cwidSpan = document.querySelector("#cwid");
-var goBack = document.querySelector(".go-back");
 var httpReq = new XMLHttpRequest();
 
 chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
     var tab = tabs[0].url;
-    var link = "https:*";
+    var link = "https";
+    console.log(tab);
     
-    console.log(JSON.stringify(tabs[1]) +"\n" +link);
-    
-    if(tab.startsWith(link)) {
-        favSubsBtn.addEventListener("click", function() {
-            chrome.tabs.executeScript({
-        code: 'document.body.style.backgroundColor="red"'
-      });
-        });
-        subListBtn.addEventListener("click", function() {
-            fadeOut(subListBtn);
-            goBack.style.display = "block";
-            printListOfCourses();
-        }); 
-    } else {
-        chrome.tabs.create({url: "https:"+tab.substring(5,100)});
-    }
+    if(tab.startsWith(link))
+    	console.log('Its already a secured link');
+    else
+    	chrome.tabs.create({url: "https:"+tab.substring(5,100)});
 });
-
-
-// window.onload = getUserName();
-// window.onload = getCwid();
